9bb8e8ebd6f4f14215d8f657cb01a435695e23e7
Harry Prabowo <harryprabowo10@gmail.com>
2019-12-06 14:15:56 +0700

    Last

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const API_URL = `http://localhost:8080/api/`
 const App = () => {
   const [started, setStart] = useState(false)
   const [player, setPlayer] = useState(0)
+  const [random, setRandom] = useState(0)
   const [mode, setMode] = useState(0)
   const [ai, setAi] = useState(1)
   const [difficulty, setDifficulty] = useState(1)
@@ -33,14 +34,18 @@ const App = () => {
         },
         body: JSON.stringify(params)
       })
+        .catch(e => {
+          console.error(e)
+        })
         .then(res => res.json())
         .then(data => {
           setAllowedMoves(data.possible_move)
           setBoard(data.state)
           countScore(data.state)
-        })
-        .catch(e => {
-          console.error(e)
+          
+          if (random) {
+            moveRandom(data.possible_move)
+          }
         })
     } else {
       setScoreBlack(0)
@@ -64,10 +69,14 @@ const App = () => {
     setScoreWhite(occurence[1])
   }
 
+  const moveRandom = (arr = allowedMoves) => {
+    moveTo(arr[Math.floor(Math.random() * arr.length)])
+  }
+
   const moveTo = move => {
     const params = {
       move: move,
-      player: turn + 1
+      player: turn + player + 1
     }
 
     fetch(API_URL + `move`, {
@@ -77,9 +86,12 @@ const App = () => {
       },
       body: JSON.stringify(params)
     })
+      .catch(e => {
+        console.error(e)
+      })
       .then(res => res.json())
       .then(data => {
-        if (data.possible_move.length === 0) {
+        if (data.possible_move.length === 0 || isNullOrUndefined(board) ? (data.possible_move.length === 0) : (board.filter(b => b === 0)).length <= 1) {
           if(scoreWhite === scoreBlack) alert(`What! A tie!?`)
           else (scoreWhite > scoreBlack ? !player : player) ? alert(`You have skills.`) :  alert('Too bad.')
 
@@ -98,18 +110,17 @@ const App = () => {
           if (mode === 1) setTurn(!turn ? 1 : 0)
 
           countScore(data.state)
+
+          if (random) moveRandom(data.possible_move)
         }
-      })
-      .catch(e => {
-        console.error(e)
       })
   }
 
   const pieceColor = num => {
     if (num === 1)
-      return player ? 'white' : 'black'
+      return 'black'
     else if (num === 2)
-      return player ? 'black' : 'white'
+      return 'white'
     else if (num === 3)
       return null
     else return ""
@@ -123,7 +134,14 @@ const App = () => {
     }
   }
 
-  const togglePlayer = () => setPlayer(!player)
+  const togglePlayer = () => {
+    setPlayer(!player)
+  }
+
+  const toggleRandom = () => {
+    setRandom(!random)
+  }
+
   const toggleMode = diff => {
     let delta = diff.deltaY > 0 ? 1 : -1
 
@@ -144,7 +162,12 @@ const App = () => {
   return (
     <div id="container">
       <div id="start-modal" className={started ? 'hide' : null}>
-        <span onWheel={togglePlayer} onClick={togglePlayer}>{player ? <>be <label style={{ color: 'white', textShadow: "-1px -1px 0 #aaa, 1px -1px 0 #aaa, -1px 1px 0 #aaa, 1px 1px 0 #aaa" }}>white</label></> : <>be <label style={{ color: 'black' }}>black</label></>}</span>
+        <span>
+          <label onWheel={toggleRandom} onClick={toggleRandom}>{random ? 'random' : 'be'}</label>
+          <label onWheel={togglePlayer} onClick={togglePlayer}>
+            {player ? <> <label style={{ color: 'white', textShadow: "-1px -1px 0 #aaa, 1px -1px 0 #aaa, -1px 1px 0 #aaa, 1px 1px 0 #aaa" }}>white</label></> : <> <label style={{ color: 'black' }}>black</label></>}
+          </label>
+        </span>
         <br className="responsive-br"/>
         <span onWheel={toggleMode} onClick={toggleMode}>against {!mode ? <label style={{ color: 'red' }}>{ai ? `AI` : 'random'}</label> : <label style={{ color: 'black' }}>people.</label>}</span>
         <br className="responsive-br"/>
@@ -161,7 +184,7 @@ const App = () => {
         ←
       </button>
       <div id="board" className={(!started ? 'hide ' : "") + (turn ? "" : 'turn')}>
-        <span className={"score " + (!!!player ? `black` : `white`)} id="score-2">{score(!!!(player ? player : !player) ? `black` : `white`)}</span>
+        <span className={"score " + (!!!player ? `black` : `white`)} id="score-2">{score(player ? `black` : `white`)}</span>
         <table>
           <tbody>
             {
@@ -182,7 +205,7 @@ const App = () => {
             }
           </tbody>
         </table>
-        <span className={"score " + (!!player ? `black` : `white`) + " "} id="score-1">{score(!!(player ? player : !player) ? `black` : `white`)}</span>
+        <span className={"score " + (!!player ? `black` : `white`) + " "} id="score-1">{score(!player ? `black` : `white`)}</span>
       </div>
       <span className=""></span>
     </div>
